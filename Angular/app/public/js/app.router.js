36143944dee792c908d8fdd5e8fa0536410e34cd
capybaraking <julie.simonin9@etu.univ-lorraine.fr>
2018-04-10 15:54:19 +0200

    Pages profil
    - css fixed (adaptation à la taille de l'écran)
    - ui routers entre les pages

--- Angular/app/public/js/app.router.js
+++ Angular/app/public/js/app.router.js
@@ -30,12 +30,40 @@ angular.module('app')
                 url: '/profil',
                 templateUrl: 'profil/profil.html',
                 controller: 'profilController'
+            })
+            .state({
+                name: 'user-enigma',
+                url: '/profil_mesEnigmes',
+                templateUrl: 'profil/profil_UserEnigma.html',
+                controller: 'profilController'
+            })
+            .state({
+                name: 'resolved-enigma',
+                url: '/profil_resolues',
+                templateUrl: 'profil/profil_UserResolved.html',
+                controller: 'profilController'
+            })
+            .state({
+                name: 'not-resolved-enigma',
+                url: '/profil_nonResolues',
+                templateUrl: 'profil/profil_UserNotResolved.html',
+                controller: 'profilController'
+            })
+            .state({
+                name: 'followed-enigma',
+                url: '/profil_suivies',
+                templateUrl: 'profil/profil_UserFollowedEnigma.html',
+                controller: 'profilController'
+            })
+            .state({
+                name: 'achievements',
+                url: '/profil_achievements',
+                templateUrl: 'profil/profil_Achievements.html',
+                controller: 'profilController'
             });
-
         $urlRouterProvider.otherwise('/');
 
 
         // Use the HTML5 History API
         // $locationProvider.html5Mode(true);
     });
-
