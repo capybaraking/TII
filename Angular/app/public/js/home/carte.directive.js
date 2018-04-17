4b7712e3c14332472742ced5dae0be6db1105421
EnagromC <EnagromC@users.noreply.github.com>
2018-04-17 23:23:44 +0200

    Formatage de la date
    J'ai changé la directive pour que la date soit en français

--- Angular/app/public/js/home/carte.directive.js
+++ Angular/app/public/js/home/carte.directive.js
@@ -4,8 +4,10 @@ angular.module("home")
 			restrict:"E", //C'est pour qu'on puisse créer une nouvelle balise <carteEnigme>
 			scope: { //C'est la liste des trucs qu'on va pouvoir utiliser dans le template
 				auteur: '=', //Ca veut dire qu'on doit le passer en paramètre : il cherche l'attribut qui s'appelle pareil dans le Controller
-				question: '='/*,
-				tags: '='*/
+				question: '=',/*,
+				*/
+				date: '='
+				//tags: '='
 			},
 			templateUrl: '/js/home/carte_enigme.html'
 		}
