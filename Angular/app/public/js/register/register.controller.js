f013f31ef462e1f3237512e7bb1b0308704297d0
capybaraking <julie.simonin9@etu.univ-lorraine.fr>
2018-04-18 14:44:28 +0200

    Inscription

--- Angular/app/public/js/register/register.controller.js
+++ Angular/app/public/js/register/register.controller.js
@@ -1,38 +1,44 @@
+"use strict";
+angular.module('app')
+.controller('registerController', ['$scope', 'inscription', function appController ($scope, inscription) {
+    $scope.username = ""; 
+    $scope.email = "";
+    $scope.password1 = "";
+    $scope.password2 = "";
 
-'use strict';
-
-angular
-	.module('app')
-	.controller('registerController', Controller);
-
-function Controller($state, AuthenticationService) {
-	var vm = this;
-
-	vm.login = register;
-
-	initController();
-
-	function initController() {
-        // reset login status
-        AuthenticationService.Logout();
-    };
-
-    function register() {
-    	
-
-
-    };
+    $scope.succes = false;
+    $scope.erreur = ''; 
 
     var password = document.getElementById("password"), password2 = document.getElementById("password2");
 
     function validatePassword(){
-      if(password.value != password2.value) {
-        password2.setCustomValidity("Les mots-de-passe que vous avez saisi ne sont pas sont identiques !");
-      } else {
+      if(password.value == password2.value) {
         password2.setCustomValidity('');
+      } else {
+        password2.setCustomValidity("Les mots-de-passe que vous avez saisi ne sont pas sont identiques !");
+        
       }
     }
 
     password.onchange = validatePassword;
     password2.onkeydown = validatePassword;
-}
+
+    $scope.reset = function(){
+        $scope.username = ""; 
+        $scope.email = ""; 
+        $scope.password1 = "";
+        $scope.password2 = "";
+    }
+
+    $scope.submit = function(){
+        console.log("Hello");
+        inscription.createAccount($scope.username, $scope.email,$scope.password1).then(function(){
+            $scope.succes = true;
+            console.log("Compte créé");
+            $scope.reset();
+        }).catch(function(erreur){
+            $scope.erreur = erreur;
+            console.log(erreur);
+        });
+    }
+}]);
--- Angular/app/public/js/services/register.service.js
+++ Angular/app/public/js/services/register.service.js
@@ -3,4 +3,20 @@ angular
 	.service("inscription", function($q, $http){ //$q c'est pour faire des promises (des ské try catch) et $http c'est pour pouvoir faire des appels AJAX (communiquer avec l'API)
 		
 
+		this.createAccount = function(username,mail,mdp){ //C'est pour créer une nouvelle énigme.
+			var maPromise = $q.defer() ;
+			if (username.length>4){
+				var data = {pseudo: username, mail: mail,password:mdp}; //La date et l'id_utilisateur seront faits sur le serveur
+				var requete = $http.post("http://localhost:3000/utilisateurs", data);
+				requete.then(function success(reponse){
+					maPromise.resolve(reponse.data);
+				}, function echec(reponse){
+					maPromise.reject("Votre compte n'a pas pu être créé : "+ reponse.status + reponse.statusText);
+				});
+			} else {
+				maPromise.reject("Votre nom d'utilisateur doit comporter au moins 3 caractères.");
+			}
+			return maPromise.promise ;
+		};
+
 	}); 
